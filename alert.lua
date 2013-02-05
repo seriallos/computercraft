a303bfa0f5421104db731130b28dd94ed2059613
Dave Hendler <sollaires@gmail.com>
2013-02-04 23:47:45 -0500

    Twilio test program and bootstrap download

---
+++ alert.lua
@@ -0,0 +1,19 @@
+
+-- simple twilio test
+
+local tArgs = { ... }
+
+if( #tArgs ~= 5 ) then
+  print( "USAGE: alert <SID> <TOKEN> <FROM> <TO> <MESSAGE>" )
+  return
+end
+
+local sid = tArgs[ 1 ]
+local token = tArgs[ 2 ]
+local from = tArgs[ 3 ]
+local to = tArgs[ 4 ]
+local msg = tArgs[ 5 ]
+
+os.loadAPI("twilio")
+
+sms( sid, token, from, to, msg )
