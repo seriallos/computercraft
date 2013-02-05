7b90f1d36cdbb7a4c6eb14598bfc53ab9be198dc
Dave Hendler <sollaires@gmail.com>
2013-02-05 00:34:43 -0500

    Still testing

--- twilio.lua
+++ twilio.lua
@@ -3,5 +3,7 @@ function sms( sid, token, from, to, msg )
   -- hack test for now
   local url = "http://davehendler.com/twiliotest.php?msg="..msg
   local request = http.get( url )
+  local response = request.readAll()
+  print( "Response = "..response )
   request.close()
 end
