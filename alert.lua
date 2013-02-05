9d53bec360efbff107b422341985e5e45fea0580
Dave Hendler <sollaires@gmail.com>
2013-02-05 00:49:24 -0500

    Closer, hardcoding a message now

--- alert.lua
+++ alert.lua
@@ -3,17 +3,14 @@
 
 local tArgs = { ... }
 
-if( #tArgs ~= 5 ) then
-  print( "USAGE: alert <SID> <TOKEN> <FROM> <TO> <MESSAGE>" )
+if( #tArgs < 2 ) then
+  print( "USAGE: alert <TO> <MESSAGE>" )
   return
 end
 
-local sid = tostring( tArgs[ 1 ] )
-local token = tostring( tArgs[ 2 ] )
-local from = tostring( tArgs[ 3 ] )
-local to = tostring( tArgs[ 4 ] )
-local msg = tostring( tArgs[ 5 ] )
+local to = tostring( tArgs[ 1 ] )
+local msg = tostring( tArgs[ 2 ] )
 
 os.loadAPI("twilio")
 
-twilio.sms( sid, token, from, to, msg )
+twilio.sms( to, "This is a hardcoded message from a Turtle" )
--- twilio.lua
+++ twilio.lua
@@ -1,7 +1,7 @@
 
-function sms( sid, token, from, to, msg )
+function sms( to, msg )
   -- hack test for now
-  local url = "http://dev.davehendler.com/twiliotest.php?msg="..msg
+  local url = "http://dev.davehendler.com/twiliotest.php?to="..to.."&msg="..msg
   local request = http.get( url )
   if request then
     request.close()
