ad143fcd63ade0656ff9945c0550e43246c70b9e
Dave Hendler <sollaires@gmail.com>
2013-02-04 22:18:15 -0500

    Program to get files from github

---
+++ github.lua
@@ -0,0 +1,32 @@
+
+local tArgs = { ... }
+
+if (#tArgs ~= 3) then
+  print( "USAGE: github get <GITHUB-PATH> <PROGRAM-NAME>" )
+  return
+end
+
+local action = tArgs[1]
+local github_path = tArgs[2]
+local program = tArgs[3]
+
+if "get" ~= action then
+  print( "Only 'get' is supported right now" )
+  return
+end
+
+if fs.exists( program ) then
+  print( "File "..program.." already exists.  No action taken" )
+  return
+end
+
+local github_url = "https://raw.github.com/"..github_path
+
+local request = http.get( github_url )
+local response = request.readAll()
+request.close()
+
+local file = fs.open( program, "w" )
+file.write( response )
+file.close()
+
