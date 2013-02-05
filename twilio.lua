2e27e16095278d2af06ed7a1d1edb67b3abddf08
Dave Hendler <sollaires@gmail.com>
2013-02-05 00:30:14 -0500

    Getting rid of readAll

--- twilio.lua
+++ twilio.lua
@@ -3,6 +3,5 @@ function sms( sid, token, from, to, msg )
   -- hack test for now
   local url = "http://davehendler.com/twiliotest.php?msg="..msg
   local request = http.get( url )
-  request.readAll()
   request.close()
 end
